802f15d4cca612195f03e02edca70013c6de26ab
Muhammad Amir Nazir <beingamirnazir@gmail.com>
2025-05-29 22:02:19 +0500

    Fix merge conflict and ensure all models are imported for Odoo 17 compatibility

--- odoo_setup_assistant/models/__init__.py
+++ odoo_setup_assistant/models/__init__.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
 from . import odoo_setup_assistant_wizard
-<<<<<<< HEAD
 from . import dependency_installer
-=======
 from . import github_repo
 from . import res_config_settings
->>>>>>> 18.0
